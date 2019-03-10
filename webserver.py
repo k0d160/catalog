5af2e588acc61b6c4023fc70bb95639cc9d12908
Jesus Ricardo Bernal <k0d160@protonmail.com>
2019-03-09 23:49:25 -0600

    feat: Add ability to delete a restaurant(object) from the database

--- webserver.py
+++ webserver.py
@@ -51,6 +51,25 @@ class webserverHandler(BaseHTTPRequestHandler):
 
                     self.wfile.write(output)
 
+            # Create form to confirm deletion
+            if self.path.endswith("/delete"):
+                restaurantIDPath = self.path.split("/")[2]
+
+                myRestaurantQuery = session.query(Restaurant).filter_by(id=restaurantIDPath).one()
+                if myRestaurantQuery:
+                    self.send_response(200)
+                    self.send_header('Content-type', 'text/html')
+                    self.end_headers()
+                    output = ""
+                    output += "<html><body>"
+                    output += "<h1>Are you sure you want to delete %s?" % myRestaurantQuery.name
+                    output += "<form method='POST' enctype='multipart/form-data' action='/restaurants/%s/delete'>" % restaurantIDPath
+                    output += "<input type='submit' value='Delete'>"
+                    output += "</form>"
+                    output += "</body></html>"
+                    self.wfile.write(output)
+
+            
             if self.path.endswith("/restaurants"):
                 restaurants = session.query(Restaurant).all()
                 output = ""
@@ -68,7 +87,7 @@ class webserverHandler(BaseHTTPRequestHandler):
                     output += "<a href='/restaurants/%s/edit'>Edit</a>" % restaurant.id
                     output += "<br>"
                     # Add Delete link
-                    output += "<a href='#'>Delete</a>"
+                    output += "<a href='/restaurants/%s/delete'>Delete</a>" % restaurant.id
                     output += "</br></br></br>"
 
                 output += "</body></html>"
@@ -83,6 +102,18 @@ class webserverHandler(BaseHTTPRequestHandler):
     # POST Method to create a new restaurant
     def do_POST(self):
         try:
+            # Delete a restaurant by clicking the delete link
+            if self.path.endswith("/delete"):
+                restaurantIDPath = self.path.split("/")[2]
+                myRestaurantQuery = session.query(Restaurant).filter_by(id=restaurantIDPath).one()
+                if myRestaurantQuery:
+                    session.delete(myRestaurantQuery)
+                    session.commit()
+                    self.send_response(301)
+                    self.send_header('Content-type', 'text/html')
+                    self.send_header('Location', '/restaurants')
+                    self.end_headers()
+
             # POST new (Edited) restaurant name
             if self.path.endswith("/edit"):
                 ctype, pdict = cgi.parse_header(self.headers.getheader('Content-type'))
