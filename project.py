baa37eaf376369feec427874e8eba6ece34b0967
Jesus Ricardo Bernal <k0d160@protonmail.com>
2019-03-10 04:19:43 -0500

    style: fixed urls

--- project.py
+++ project.py
@@ -5,15 +5,16 @@ from database_setup import Base, Restaurant, MenuItem
 
 app = Flask(__name__)
 
-
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
 
 
+@app.route('/')
 @app.route('/restaurants/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
     DBSession = sessionmaker(bind=engine)
     session = DBSession()
+
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
     output = ''
@@ -25,23 +26,23 @@ def restaurantMenu(restaurant_id):
         output += i.description
         output += '</br>'
         output += '</br>'
+
     return output
 
 
-@app.route('/restaurant/<int:restaurant_id>/new/')
+@app.route('/restaurants/<int:restaurant_id>/new/')
 def newMenuItem(restaurant_id):
     return "page to create a new menu item."
 
 
-@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit/')
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/')
 def editMenuItem(restaurant_id, menu_id):
     return "page to edit a menu item."
 
 
-@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete/')
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/')
 def deleteMenuItem(restaurant_id, menu_id):
     return "page to delete a menu item."
-
 
 if __name__ == '__main__':
     app.debug = True
