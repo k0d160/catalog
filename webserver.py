e2a77c50659c6ed71f5be8fa2d4179ca13f019e4
Jesus Ricardo Bernal <k0d160@protonmail.com>
2019-03-09 21:56:52 -0600

    refactor: remove useles POST method

--- webserver.py
+++ webserver.py
@@ -25,6 +25,10 @@ class webserverHandler(BaseHTTPRequestHandler):
                 output += "<html><body>"
                 for restaurant in restaurants:
                     output += restaurant.name
+                    output += "<br>"
+                    output += "<a href='#'>Edit</a>"
+                    output += "<br>"
+                    output += "<a href='#'>Delete</a>"
                     output += "</br></br></br>"
 
                 output += "</body></html>"
@@ -35,28 +39,7 @@ class webserverHandler(BaseHTTPRequestHandler):
         except IOError:
             self.send_error(404, "File Not Found %s" % self.path)
 
-    def do_POST(self):
-        try:
-            self.send_response(301)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            ctype, pdict = cgi.parse_header(
-                self.headers.getheader('content-type'))
-            if ctype == 'multipart/form-data':
-                fields = cgi.parse_multipart(self.rfile, pdict)
-                messagecontent = fields.get('message')
-            output = ""
-            output += "<html><body>"
-            output += " <h2> Okay, how about this: </h2>"
-            output += "<h1> %s </h1>" % messagecontent[0]
-            output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
-            output += "</body></html>"
-            self.wfile.write(output)
-            print output
-        except:
-            pass
-
-
+    
 def main():
     try:
         port = 8080
