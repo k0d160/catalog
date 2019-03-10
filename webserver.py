d198d3e5657ae1d28891800ca245d6899bad1fb8
Jesus Ricardo Bernal <k0d160@protonmail.com>
2019-03-09 22:45:29 -0600

    feat: Add ability to reate a new restaurant

--- webserver.py
+++ webserver.py
@@ -16,18 +16,39 @@ session = DBSession()
 class webserverHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         try:
+            if self.path.endswith("/restaurants/new"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                # Form to create a new restaurant 
+                output = ""
+                output += "<html><body>"
+                output += "<h1>Create a New Restaurant</h1>"
+                output += "<form method='POST' enctype='multipart/form-data' action='/restaurants/new'>"
+                output += "<input name='newRestaurantName' type='text' placeholder='New Restaurant Name'>"
+                output += "<input type='submit' value='create'>"
+                output += "</form></body></html>"
+                self.wfile.write(output)
+                return
+
+
             if self.path.endswith("/restaurants"):
                 restaurants = session.query(Restaurant).all()
                 output = ""
+                # Link to create a new item
+                output += "<a href = '/restaurants/new'> Welcome, creat a new restaurant </a></br></br></br>"
+
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
                 output += "<html><body>"
                 for restaurant in restaurants:
                     output += restaurant.name
+                    # Add Edit link
                     output += "<br>"
                     output += "<a href='#'>Edit</a>"
                     output += "<br>"
+                    # Add Delete link
                     output += "<a href='#'>Delete</a>"
                     output += "</br></br></br>"
 
@@ -39,6 +60,28 @@ class webserverHandler(BaseHTTPRequestHandler):
         except IOError:
             self.send_error(404, "File Not Found %s" % self.path)
 
+
+    # POST Method to create a new restaurant
+    def do_POST(self):
+        try:
+            if self.path.endswith("/restaurants/new"):
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    messagecontent = fields.get('newRestaurantName')
+
+                    # Create new Restaurant Object
+                    newRestaurant = Restaurant(name=messagecontent[0])
+                    session.add(newRestaurant)
+                    session.commit()
+
+                    self.send_response(301)
+                    self.send_header('Content-type', 'text/html')
+                    self.send_header('Location', '/restaurants')
+                    self.end_headers()
+
+        except:
+            pass
     
 def main():
     try:
